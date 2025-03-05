2e818c6fd7d2c88b7363d3ee3edb0f8dc1b1fb1b
haroldCoder <haroldc2005@gmail.com>
2025-03-05 14:33:16 -0500

    feature: change logic

--- cypress/constants/areas.ts
+++ cypress/constants/areas.ts
@@ -1,10 +1,15 @@
 export class Areas{
     percentaje: number;
     name: string;
+    correct_answers: number = 0;
 
 
     constructor(percentaje: number, name: string){
         this.percentaje = percentaje;
         this.name = name;
+    }
+
+    asignAswers(correct_answers: number){
+        this.correct_answers = correct_answers;
     }
 }
